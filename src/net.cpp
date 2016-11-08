f0dd4a38ad2e9a231f4281acfce9e091efbfb220
presstab <presstab1337@gmail.com>
2016-11-08 09:15:39 -0700

    Add presstab seed

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"", ""}, //Main Qibuck Seed Node
+    {"presstab seed", "qbkseed.presstab.pw"}, //Main Qibuck Seed Node
 };
 
 void ThreadDNSAddressSeed(void* parg)
